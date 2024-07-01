899122398f65de16b6772b41f9bce6396f3a6785
pradhansmriti <smritipradhan@brandeis.edu>
2024-07-01 09:10:45 -0400

    run local parameter file

---
+++ with_drug_binding/run_locally.sh
@@ -0,0 +1,2 @@
+./source/assemble seed epsilon0 kappa0 kappaPhi0 theta0 theta1 LnK muCd ks0 dmu dummydg mudrug gdrug kd0 dg12 dg01 dg20 dg33 dg00 dgother
+./source/assemble 85565 4200.000 40.000 800.000 0.240 0.480 -9.850 -11.500 0.020000 -5.500 0.100 0.000 0.900 0.006000 0.300 0.100 -0.100 0.000 -0.600 -0.950
